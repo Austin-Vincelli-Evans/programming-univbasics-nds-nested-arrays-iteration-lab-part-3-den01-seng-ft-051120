11f4cfb067089a8a66d77d3c9a9d7d4e9f3b49d5
Austin Evans <unity7591@gmail.com>
2020-04-28 17:47:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,16 @@
 def join_nested_strings(src)
   row_index = 0 
-  string_story = []
+  final_story = []
   while row_index < src.count do
     element_index = 0 
+    string_story = []
     while element_index < src[row_index].count do
      if src[row_index][element_index].is_a? Integer
        element_index+=1
      else
       
       string_story = src[row_index][element_index]
-      
+      final_story << string_story
       element_index+=1 
       end
     end
